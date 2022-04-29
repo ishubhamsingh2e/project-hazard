30fd615d9e3e2004edc54f8b94f7a7a73c5a7f84
Shubham Singh <sb2001nov@gmail.com>
2022-04-29 20:29:44 +0530

    cards

---
+++ app.py
@@ -0,0 +1,22 @@
+from flask import Flask
+import os
+import random
+app = Flask(__name__)
+
+PATH = "data/card"
+
+try:
+    DATA.remove('red')
+except:
+    pass
+
+def cars() -> tuple:
+    items = []
+    items.append()
+
+@app.route("/")
+def hello_world():
+    return "<p>Hello, World!</p>"
+
+if __name__ == "__main__":
+    app.run()
