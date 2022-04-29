9145545de19742c9a2e98d663bac0606cbf578c7
Shubham Singh <sb2001nov@gmail.com>
2022-04-29 20:29:19 +0530

    re-formatted

--- script/a.py
+++ script/a.py
@@ -3,10 +3,11 @@ import numpy as np
 import os
 from PIL import Image
 
-path = "data/original"
+path = "data/card/red"
 data = os.listdir(path=path)
+# data.remove("red")
 flg = 1
 
 for i in data:
-    os.rename(f"{path}/{i}", f"{path}/{flg}.jpg")
+    os.rename(f"{path}/{i}", f"new/{flg}.jpg")
     flg += 1
