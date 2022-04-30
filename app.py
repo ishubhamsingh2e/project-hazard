926fb15e0d4b47873aa6d5433a5d87f71553b7c7
Shubham Singh <sb2001nov@gmail.com>
2022-04-30 14:07:17 +0530

    little change,API completed

--- app.py
+++ app.py
@@ -1,22 +1,52 @@
 from flask import Flask
+from flask import jsonify, request
+
 import os
 import random
 app = Flask(__name__)
 
-PATH = "data/card"
+IMAGE_PATH = "https://raw.githubusercontent.com/ishubhamsingh2e/project-hazard/main/data/card/"
 
-try:
-    DATA.remove('red')
-except:
-    pass
+def cards(red:bool=0,chance:int=10) -> dict:
+    check = [1,2,1]
+    items = {
+        "red": red
+    }
+    rand1, rand2, rand3 = 0,0,0
 
-def cars() -> tuple:
-    items = []
-    items.append()
+    rand1 = random.randint(1, 322)
+    rand2 = random.randint(1, 322)
+    check.append(rand1)
+    check.append(rand2)
 
-@app.route("/")
+    items["1"]  = f"{IMAGE_PATH}{rand1}.jpg"
+    items["2"] = f"{IMAGE_PATH}{rand2}.jpg"
+    
+    if random.randint(0, chance-1) == 0 and red == 0:
+        rand3 = random.randint(1, 38)
+        check.append(rand3)
+        items["3"] = f"{IMAGE_PATH}red/{rand3}.jpg"
+        items["red"] = 1
+
+    elif red == 1:
+        rand3 = random.randint(1, 38)
+        check.append(rand3)
+        items["3"] =  f"{IMAGE_PATH}red/{rand3}.jpg"
+
+    else:
+        rand3 = random.randint(1, 322)
+        check.append(rand3)
+        items["3"] =  f"{IMAGE_PATH}{rand3}.jpg"
+
+    return items
+
+@app.route("/", methods=['GET'])
 def hello_world():
-    return "<p>Hello, World!</p>"
+    args = request.args
+    red = args.get("red", default=0, type=int)
+    chance = args.get("chance", default=10, type=int)
+    data = cards(red=red, chance=chance)
+    return jsonify(data)
 
 if __name__ == "__main__":
-    app.run()
+    app.run(debug=True)
