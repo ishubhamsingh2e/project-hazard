7aef01c6b607517ea7bd2c8f249477f7a6a61265
Sarthak Goel <63970210+sarty-definite@users.noreply.github.com>
2022-05-01 07:39:30 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,2 +1,20 @@
-if __name__ == "__main__":
-    pass
+from discord.ext import commands
+import os
+
+bot_token = "Enter bot token here"
+bot_prefix = '*'
+
+
+client = commands.Bot(command_prefix=bot_prefix, help_command=None)
+
+
+@client.event
+async def on_ready():
+    print("I am working")
+
+
+for filename in os.listdir('./cogs'):
+    if filename.endswith(".py"):
+        client.load_extension(f'cogs.{filename[:-3]}')
+
+client.run(bot_token)
